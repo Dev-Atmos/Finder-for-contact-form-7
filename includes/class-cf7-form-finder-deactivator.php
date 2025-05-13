790bd17e026dbe35f28a65aaed73c23041fac3c5
Dipakkumar Prajapati <166278432+Dev-Atmos@users.noreply.github.com>
2025-05-13 09:10:48 +0100

    Add files via upload

---
+++ includes/class-cf7-form-finder-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://dentalfocus.co.uk
+ * @since      1.0.0
+ *
+ * @package    Cf7_Form_Finder
+ * @subpackage Cf7_Form_Finder/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cf7_Form_Finder
+ * @subpackage Cf7_Form_Finder/includes
+ * @author     Dental Focus <info@test.com>
+ */
+class Cf7_Form_Finder_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
